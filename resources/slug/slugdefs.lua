7e68c59cefc2efa4ed67d05de50f30489ca23bd6
GayCodeGal <gaycodegal@github.com>
2018-06-04 16:11:51 -0400

    finished spriting re-work

--- resources/slug/slugdefs.lua
+++ resources/slug/slugdefs.lua
@@ -58,10 +58,75 @@ return {
 			   }
 			}
 		 }
-   }},
+			 },
+	  {
+		 name = "tails",
+		 tilewidth = 56,
+		 tileheight = 56,
+		 spacing = 0,
+		 margin = 0,
+		 image = "tailsprites.png",
+		 imagewidth = 168,
+		 imageheight = 112,
+		 tileoffset = {
+			x = 0,
+			y = 0
+		 },
+		 grid = {
+			orientation = "orthogonal",
+			width = 56,
+			height = 56
+		 },
+		 properties = {},
+		 terrains = {},
+		 tilecount = 6,
+		 tiles = {
+			{
+			   id = 0,
+			   properties = {
+				  ["color"] = "008cff"
+			   }
+			},
+			{
+			   id = 1,
+			   properties = {
+				  ["color"] = "008cff"
+			   }
+			},
+			{
+			   id = 2,
+			   properties = {
+				  ["color"] = "008cff"
+			   }
+			},
+			{
+			   id = 3,
+			   properties = {
+				  ["color"] = "ff7200"
+			   }
+			},
+			{
+			   id = 4,
+			   properties = {
+				  ["color"] = "ff7200"
+			   }
+			},
+			{
+			   id = 5,
+			   properties = {
+				  ["color"] = "ff7200"
+			   }
+			}
+		 }
+	  }
+   },
    slugs={
 	  test = {
-		 tile=1
-	  }
+		 tiles = {1,7}
+	  },
+	  test2 = {
+		 tiles = {4, 10}
+	  },
+	  
    }
 }
