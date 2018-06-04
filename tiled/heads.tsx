21af4cd6d14d891715482dc3bfbae0796d5d9cbe
GayCodeGal <gaycodegal@github.com>
2018-06-04 14:50:07 -0400

    added tiled source

---
+++ tiled/heads.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="heads" tilewidth="56" tileheight="56" tilecount="6" columns="3">
+ <image source="headsprites.png" width="168" height="112"/>
+ <tile id="0">
+  <properties>
+   <property name="color" value="008cff"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="color" value="008cff"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="color" value="008cff"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="color" value="ff7200"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="color" value="ff7200"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="color" value="ff7200"/>
+  </properties>
+ </tile>
+</tileset>
