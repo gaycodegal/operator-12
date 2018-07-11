4cea8f4ce77184521ca05b4e30460bb2345cc6ac
stephoro <soro@stevens.edu>
2018-07-10 20:35:18 -0400

    textbox reverse now works

--- resources/ui/TextBox.lua
+++ resources/ui/TextBox.lua
@@ -8,13 +8,7 @@ function TextBox.new(self)
    local layout = self.layout
    self.j = layout.e.justify or 1
    self.fg = layout.e.fg or {255,255,255,255}
-   self.CIL = Text.charsInLine
-   self.sub = Text.sub
-   print(layout.e.direction)
-   if layout.e.direction == 1 then
-	  self.CIL = Text.charsInLineRev
-	  self.sub = Text.subRev
-   end
+   self.direction = layout.e.direction
    self:resize()
    return self
 end
@@ -34,19 +28,27 @@ function TextBox:resize()
    self:destroy()
    self.rect = self.layout:rect()
    local r = self.rect
-   local s, l, h = Text.textbox(
+   local text = self.text
+   if self.direction == 2 then
+	  local len, h = Text.charsInTextbox(
 	  self.text,
+	  r[3],
+	  r[4],
+	  2)
+	  text = string.sub(text, #text - len + 1, -1)
+   end
+   local s, l, h = Text.textbox(
+	  text,
 	  self.j,
 	  r[3],
 	  r[4],
 	  self.fg,
-	  self.CIL,
-	  self.sub)
+	  1)
    self.displaying = l
    local bg = self.bg or {255,128,128,255}
    local s2 = Surface.newBlank(r[3], r[4])
    Surface.fill(s2, 0, 0, r[3], r[4], bg[1], bg[2], bg[3], bg[4])
-   Surface.blit(s2, s, 0, (r[4] - h) // 2)
+   Surface.blit(s2, s, 0, 0)
    self.tex = Surface.textureFrom(s2)
    self.spr = Sprite.new(self.tex, r[1], r[2], r[3], r[4], 0, 0) -- sprite
    Surface.destroy(s)
