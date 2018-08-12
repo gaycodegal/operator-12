a7b91dc46ce5452e43357a30809d3617d2a02946
GayCodeGal <gaycodegal@github.com>
2018-08-12 11:56:26 -0400

    feature update

--- resources/ui/styles/list-button.lua
+++ resources/ui/styles/list-button.lua
@@ -5,5 +5,19 @@ return {
 	  h="h",
 	  x="p.x",
 	  y="y"
+   },
+   lbContainer={
+	  vars={"p=self.p"},
+	  w="p.w",
+	  h="p.h",
+	  x="p.x",
+	  y="p.y"
+   },
+   lbChild={
+	  vars={"p=self.p","h=d.h","th=(d.n * h + (d.n - 1) * d.s)","a=d.align","y=p.y + ((a==3 and 0) or th)"},
+	  w="p.w",
+	  h="p.h-th",
+	  x="p.x",
+	  y="y"
    }
 }
