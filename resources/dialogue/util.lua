47041fb442c67ba42bbc94587a22eadbfd8b24b5
stephoro <soro@stevens.edu>
2018-08-25 16:51:26 -0400

    docs

--- resources/dialogue/util.lua
+++ resources/dialogue/util.lua
@@ -1,15 +1,34 @@
 DUtil = {}
 
+--[[
+Go to point
+
+@param point 
+
+@return callback closure
+]]
 function DUtil.go(point)
    return function(self)
 	  self:go(self.dialogue, point)
    end
 end
 
+--[[
+reload current point
+
+@param self 
+]]
 function DUtil.reload(self)
    self:go(self.dialogue, self.point)
 end
 
+--[[
+load a view controller in the global scope
+
+@param name 
+
+@return callback closure
+]]
 function DUtil.loadGlobalController(name)
    return function (self)
 	  End()
