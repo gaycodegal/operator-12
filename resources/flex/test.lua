38199829681c6ce46b0ecaacb1bbdfe4b9d21f15
GayCodeGal <gaycodegal@github.com>
2018-12-23 20:01:23 -0500

    flex visual test

--- resources/flex/test.lua
+++ resources/flex/test.lua
@@ -1,34 +1,36 @@
 require("util")
+local isMain = Util.isMain()
 require("data/save")
 require("flex/flex")
-local isMain = Util.isMain()
 local F = Flex
 
 function Flex.Start()
    print("hi")
-   err, res = xpcall(Flex.calculateRects,
-		     debug.traceback,
-		     {axis=2,
-		      children={
-			 {size={1,"w"},
-			  axis=1,
-			  children={
-			     {size={50,"dp"}}
-			  }
-			 },
-			 {size={1,"w"}},
-		     }},
-		     {0,0,100,100})
-   print(res)
-   print(table.tostring(res))
+   cells = dofile("flex/test.layout.lua")
+   rects = Flex.calculateRects(cells, {0,0,SCREEN_WIDTH,SCREEN_HEIGHT})
+   print(rects)
+   print(table.tostring(rects))
+   things = Flex.new(cells, rects)
+   print("things")
+   Flex.setData(things,
+		rects
+   )
+   print("end")
 end
 
 function Flex.Update()
-   Update=static.quit()
+   --Update=static.quit()
+   Flex.draw(things)
+end
+
+function Flex.Resize(w, h)
+   Util.Resize(w,h)
+   rects = Flex.calculateRects(cells, {0,0,SCREEN_WIDTH,SCREEN_HEIGHT})
+   Flex.setRects(things, rects)
 end
 
 function Flex.End()
-   
+   Flex.destroy(things)
 end
 
 Util.try(isMain, F)
