9e958fd4f599a141c9d4253ab96e3774a32bc7e3
GayCodeGal <gaycodegal@github.com>
2019-02-04 22:05:53 -0500

    start of dialog refactor

---
+++ resources/dialogue/layout.lua
@@ -0,0 +1,13 @@
+return {
+   axis=vertical,
+   children={
+	  {class=UIButton,
+		   name="text",
+		   text="testing",
+		   color="ff0000",
+		   size={1,"w"}},
+	  {class=UIList,
+	   name = "actions",
+	   size={100,"dp"}},
+   },
+}
