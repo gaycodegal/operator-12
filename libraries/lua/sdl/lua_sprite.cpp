1e915b921cb40ddddc3c3c21ca30151be965c05b
gaycodegal <sarahmavenoro@gmail.com>
2020-02-02 00:45:43 -0500

    updating sprite with bind

--- libraries/lua/sdl/lua_sprite.cpp
+++ libraries/lua/sdl/lua_sprite.cpp
@@ -1,86 +1,57 @@
 #include "lua_sprite.h"
 
 /**
+   creates new surface from image source
+
+   @lua-constructor
+   @lua-name new
+   @lua-arg tex: Class SDL_Texture
+   @lua-arg x: int
+   @lua-arg y: int
+   @lua-arg w: int
+   @lua-arg h: int
+   @lua-arg sx: int
+   @lua-arg sy: int
+   @lua-return Class Sprite
+ */
+static Sprite* new_sprite(SDL_Texture* tex, lua_Integer x, lua_Integer y, lua_Integer w, lua_Integer h, lua_Integer sx, lua_Integer sy) {
+  Sprite* s = new Sprite();
+  s->init(tex, x, y, w, h, sx, sy);
+  return s;
+}
+
+/**
    draw a sprite
 
-   @lua-meta
    @lua-name draw
+   @lua-arg self: Class Sprite
+   @lua-arg x: int
+   @lua-arg y: int
  */
-static int l_draw_sprite(lua_State *L) {
-  Sprite *s;
-  int x, y;
-  if (!lua_isnumber(L, -1)) {
-    lua_pop(L, 3);
-    return 0;
-  }
-  y = lua_tointeger(L, -1);
-  lua_pop(L, 1);
-
-  if (!lua_isnumber(L, -1)) {
-    lua_pop(L, 2);
-    return 0;
-  }
-  x = lua_tointeger(L, -1);
-  lua_pop(L, 1);
-
-  if (!lua_isuserdata(L, -1)) {
-    lua_pop(L, 1);
-    return 0;
-  }
-  s = *(Sprite **)lua_touserdata(L, -1);
-  lua_pop(L, 1);
-  s->draw(x, y);
-  return 0;
+static int l_draw_sprite(Sprite* self, lua_Integer x, lua_Integer y) {
+  self->draw(x, y);
 }
 
 /**
    free a sprite
 
-   @lua-meta
    @lua-name destroy
+   @lua-arg self: Delete Sprite
  */
-static int l_free_sprite(lua_State *L) {
-  Sprite *s;
-  if (!lua_isuserdata(L, -1)) {
-    lua_pop(L, 1);
-    return 0;
-  }
-  s = *(Sprite **)lua_touserdata(L, -1);
-  lua_pop(L, 1);
-  delete s;
-  return 0;
+static int l_free_sprite(Sprite* self) {
+  delete self;
 }
 
 /**
    move a sprite
 
-   @lua-meta
    @lua-name move
+   @lua-arg self: Class Sprite
+   @lua-arg x: int
+   @lua-arg y: int
  */
-static int l_move_sprite(lua_State *L) {
-  // printLuaStack(L, "move_sprite");
-  int x, y;
-  Sprite *s;
-  if (!lua_isnumber(L, -1)) {
-    lua_pop(L, 3);
-    return 0;
-  }
-  y = lua_tointeger(L, -1);
-  lua_pop(L, 1);
-  if (!lua_isnumber(L, -1)) {
-    lua_pop(L, 2);
-    return 0;
-  }
-  x = lua_tointeger(L, -1);
-  lua_pop(L, 1);
-  if (!lua_isuserdata(L, -1)) {
-    lua_pop(L, 1);
-    return 0;
-  }
-  s = *reinterpret_cast<Sprite **>(lua_touserdata(L, -1));
-  lua_pop(L, 1);
-  s->move(x, y);
-  return 0;
+static void l_move_sprite(Sprite* self, lua_Integer x, lua_Integer y) {
+  self->move(x, y);
 }
 
 /**
@@ -110,27 +81,10 @@ static int l_size_sprite(lua_State *L) {
     return 0;
   }
   s = *reinterpret_cast<Sprite **>(lua_touserdata(L, -1));
+  if (s == NULL) {
+    return 0;
+  }
   lua_pop(L, 1);
   s->size(w, h);
   return 1;
-}
-
-/**
-   creates new surface from image source
-
-   @lua-constructor
-   @lua-name new
-   @lua-arg tex: Class SDL_Texture
-   @lua-arg x: int
-   @lua-arg y: int
-   @lua-arg w: int
-   @lua-arg h: int
-   @lua-arg sx: int
-   @lua-arg sy: int
-   @lua-return Class Sprite
- */
-static Sprite* new_sprite(SDL_Texture* tex, lua_Integer x, lua_Integer y, lua_Integer w, lua_Integer h, lua_Integer sx, lua_Integer sy) {
-  Sprite* s = new Sprite();
-  s->init(tex, x, y, w, h, sx, sy);
-  return s;
 }
